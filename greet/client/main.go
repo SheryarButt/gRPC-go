e95d3b0d3ae38519bc36c3034ada451d265cbbfa
Muhammad Sheryar Butt <muhammad.sheryar@teo-intl.com>
2022-06-07 13:59:29 +0500

    Bypassed SSL requirement for client

--- greet/client/main.go
+++ greet/client/main.go
@@ -4,13 +4,14 @@ import (
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var addr string = "localhost:50051"
 
 func main() {
 
-	conn, err := grpc.Dial(addr)
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
